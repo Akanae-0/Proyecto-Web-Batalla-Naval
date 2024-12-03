4c84fdc678edeaf776ce9431e3b7710253761b63
Akanae <aaronalfonsobg@gmail.com>
2024-12-02 21:28:03 -0400

    Added Aside JS Script

--- Laboratorio Programacion Orientada a la Web/Script.js
+++ Laboratorio Programacion Orientada a la Web/Script.js
@@ -1,42 +1,74 @@
-var game_section = document.getElementById("Seccion_Tableros");
+const gameSection = document.getElementById("Seccion_Tableros");
+const asideSection = document.getElementById("Barcos_Lado")
 
 // Create a grid, for example, 5x5
-var rows = 11;
-var columns = 11;
-var numero_tableros = 2;
-var game_board_player_title = "";
+const rows = 11;
+const columns = 11;
+const numeroTableros = 2;
+const numeroBarcos = 5;
+let gameBoardPlayerTitle = "";
+const filaToLetra = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "j"];
+const barcosJuego = [
+    {
+        name: "Portaaviones",
+        source: "BattleShip_Lab_Assets/Portaaviones_5_Cas.png",
+        spaces: 5
+    },
+    {
+        name: "Acorazado",
+        source: "BattleShip_Lab_Assets/Acorazado_4_Cas.png",
+        spaces: 4
+    },
+    {
+        name: "Crucero",
+        source: "BattleShip_Lab_Assets/Crucero_3_Cas.png",
+        spaces: 3
+    },
+    {
+        name: "Submarino",
+        source: "BattleShip_Lab_Assets/Submarino_3_Cas.png",
+        spaces: 3
+    },
+    {
+        name: "Destructor",
+        source: "BattleShip_Lab_Assets/Destructor_2_Cas.png",
+        spaces: 2
+    }
+]
 
-for (var x = 0; x < numero_tableros; x++) {
 
-    var unidad_tablero_creacion = document.createElement("section");
-    unidad_tablero_creacion.classList.add("Board_Unity");
+
+for (let x = 0; x < numeroTableros; x++) {
+
+    let unidadTableroCreacion = document.createElement("section");
+    unidadTableroCreacion.classList.add("Board_Unity");
 
     if (x == 0){
-        game_board_player_title = "JUGADOR";
+        gameBoardPlayerTitle = "JUGADOR";
     } else{
-        game_board_player_title = "OPONENTE";
+        gameBoardPlayerTitle = "OPONENTE";
     }
 
-    unidad_tablero_creacion.setAttribute('id', "TABLERO_" + game_board_player_title);
-    var id_Unidad_Tablero = unidad_tablero_creacion.id;
+    unidadTableroCreacion.setAttribute('id', "TABLERO_" + gameBoardPlayerTitle);
+    let idUnidadTablero = unidadTableroCreacion.id;
 
     // Create game board title
-    var title_card_board = document.createElement("p");
-    title_card_board.classList.add("Board_Title_Card");
-    title_card_board.innerHTML = game_board_player_title;
+    let titleCardBoard = document.createElement("p");
+    titleCardBoard.classList.add("Board_Title_Card");
+    titleCardBoard.innerHTML = gameBoardPlayerTitle;
 
-    unidad_tablero_creacion.appendChild(title_card_board);
+    unidadTableroCreacion.appendChild(titleCardBoard);
 
     // Create game board format
-    var tablero_creacion = document.createElement("section");
-    tablero_creacion.classList.add("Board");
+    let tableroCreacion = document.createElement("section");
+    tableroCreacion.classList.add("Board");
 
     // Loop to create grid items and append them to the body
 
 
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < columns; j++) {
-            var gridItem = document.createElement("div");
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let gridItem = document.createElement("div");
             gridItem.classList.add("casilla");
             
             if (i == 0 && j == 0) gridItem.classList.add("casilla_vacia");
@@ -114,14 +146,23 @@ for (var x = 0; x < numero_tableros; x++) {
                             break;
                     }
                 }
-            } else gridItem.setAttribute('id', "Casilla_"+i+","+j+"_"+id_Unidad_Tablero);
+            } else gridItem.setAttribute('id', "Casilla_"+filaToLetra[i]+","+j+"_"+idUnidadTablero);
 
             // Append the grid item to the body
-            tablero_creacion.appendChild(gridItem);
+            tableroCreacion.appendChild(gridItem);
         }
     }
     
-    unidad_tablero_creacion.appendChild(tablero_creacion);
+    unidadTableroCreacion.appendChild(tableroCreacion);
 
-    game_section.appendChild(unidad_tablero_creacion);
+    gameSection.appendChild(unidadTableroCreacion);
+}
+
+for (let barco of barcosJuego){
+    let barcoCreando = document.createElement("img");
+    barcoCreando.setAttribute("id", barco.name);
+    barcoCreando.setAttribute("src", barco.source);
+    barcoCreando.setAttribute("alt", barco.name);
+    barcoCreando.classList.add("Barco");
+    asideSection.appendChild(barcoCreando);
 }
